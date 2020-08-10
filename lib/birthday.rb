da0557fd64138e50e339d4027d49a08af5367cf4
Noah Eakin <noaheakin@gmail.com>
2020-08-10 19:57:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -3,3 +3,9 @@
 #   "Sarah" => 6,
 #   "Amanda" => 27
 # }
+
+def happy_birthday (birthday_kids)
+  birthday_kids.each do |name, age|
+    p "Happy Birthday #{name}! You are now #{age} years old!"
+  end
+end
